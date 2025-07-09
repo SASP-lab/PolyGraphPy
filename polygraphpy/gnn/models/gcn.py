74398904e6855f65b31b0b0bf10fd836f2a640cf
Gabriel Duarte <dacos033@umn.edu>
2025-07-09 13:41:51 -0500

    adding chain size to the embbeding

--- polygraphpy/gnn/models/gcn.py
+++ polygraphpy/gnn/models/gcn.py
@@ -17,11 +17,11 @@ class GCN(torch.nn.Module):
         self.conv2 = GCNConv(conv_hidden_channels, conv_hidden_channels, normalize=True)
         self.conv3 = GCNConv(conv_hidden_channels, conv_hidden_channels, normalize=True)
 
-        self.lin1 = Linear(conv_hidden_channels, mlp_hidden_channels)
+        self.lin1 = Linear(conv_hidden_channels+1, mlp_hidden_channels) #adding the chain size to the embbeding
 
         self.output = Linear(mlp_hidden_channels, 1)
     
-    def forward(self, x, edge_index, edge_weight, batch):
+    def forward(self, x, edge_index, edge_weight, batch, chain_size):
         h = self.conv1(x, edge_index=edge_index, edge_weight=edge_weight)
         h = h.tanh()
         h = self.conv2(h, edge_index=edge_index, edge_weight=edge_weight)
@@ -30,6 +30,9 @@ class GCN(torch.nn.Module):
         h = h.tanh()
 
         h = global_mean_pool(h, batch)
+
+        chain_size = chain_size.unsqueeze(1)    # shape: [2, 1]
+        h = torch.cat([h, chain_size], dim=1)  # shape: [2, 121]
 
         h = self.lin1(h)
         h = h.tanh()
