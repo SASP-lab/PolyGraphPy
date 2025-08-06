87b4d17a8cd9e86e20957ba33c423f53b50391b9
Joao Gabriel Duarte <joaogabrielduarte96@gmail.com>
2025-08-06 15:07:16 -0500

    refactoring

--- polygraphpy/dftb/smiles_to_xyz.py
+++ polygraphpy/dftb/smiles_to_xyz.py
@@ -9,6 +9,8 @@ from rdkit.Chem import AllChem
 from tqdm import tqdm
 from joblib import Parallel, delayed
 
+from polygraphpy.utils.make_dummy_atom import replace_first_acrylate_cce
+
 # Set up logging
 logging.getLogger('rdkit').setLevel(logging.ERROR)
 logging.basicConfig(filename='xyz_generation_errors.log', level=logging.INFO,
@@ -117,63 +119,6 @@ class PolymerXyzGenerator(XyzGeneratorBase):
         Chem.SanitizeMol(mol, catchErrors=True)
         return mol.HasSubstructMatch(Chem.MolFromSmarts('C=C-C(=O)O'))
     
-    def replace_first_acrylate_cce(self, smiles: str, contains_br: bool) -> str:
-        """Replace C=C in acrylate group with single bond and add Br atoms."""
-        # Convert SMILES to molecule
-        mol = Chem.MolFromSmiles(smiles)
-        if mol is None:
-            raise ValueError("Invalid SMILES string")
-        
-        # Flexible SMARTS pattern for acrylate group
-        acrylate_pattern = Chem.MolFromSmarts('[C:1]=[C:2][C:3](=O)[O:4]')
-        if not mol.HasSubstructMatch(acrylate_pattern):
-            raise ValueError("No acrylate group found")
-
-        # Get the first acrylate match
-        matches = mol.GetSubstructMatches(acrylate_pattern)
-        match = matches[0]
-        c1_idx, c2_idx = match[0], match[1]  # Indices of [C:1]=[C:2]
-
-        # Existing verification: Check the bond between c1_idx and c2_idx
-        bond = mol.GetBondBetweenAtoms(c1_idx, c2_idx)
-        if bond is None or bond.GetBondType() != Chem.BondType.DOUBLE:
-            raise ValueError("Expected double bond not found in acrylate group")
-
-        # Additional bond-based verification (inspired by provided snippet)
-        found_cc_double = False
-        for bond in mol.GetBonds():
-            if bond.GetIdx() == bond.GetIdx() and bond.GetBondType() == Chem.BondType.DOUBLE:
-                atom1 = bond.GetBeginAtom()
-                atom2 = bond.GetEndAtom()
-                if (atom1.GetIdx() == c1_idx and atom2.GetIdx() == c2_idx) or \
-                (atom1.GetIdx() == c2_idx and atom2.GetIdx() == c1_idx):
-                    if atom1.GetSymbol() == 'C' and atom2.GetSymbol() == 'C':
-                        found_cc_double = True
-                        break
-        if not found_cc_double:
-            raise ValueError("No carbon-carbon double bond found in acrylate group at matched indices")
-
-        # Modify the molecule
-        rw_mol = Chem.RWMol(mol)
-        rw_bond = rw_mol.GetBondBetweenAtoms(c1_idx, c2_idx)
-        rw_bond.SetBondType(Chem.BondType.SINGLE)
-        if not contains_br:
-            br1 = rw_mol.AddAtom(Chem.Atom('Br'))
-            br2 = rw_mol.AddAtom(Chem.Atom('Br'))
-        else:
-            br1 = rw_mol.AddAtom(Chem.Atom('I'))
-            br2 = rw_mol.AddAtom(Chem.Atom('I'))
-        rw_mol.AddBond(c1_idx, br1, Chem.BondType.SINGLE)
-        rw_mol.AddBond(c2_idx, br2, Chem.BondType.SINGLE)
-        
-        # Sanitize the modified molecule
-        try:
-            Chem.SanitizeMol(rw_mol)
-        except Chem.MolSanitizeException as e:
-            raise ValueError("Failed to sanitize modified molecule: " + str(e))
-        
-        return Chem.MolToSmiles(rw_mol, isomericSmiles=True)
-    
     def build_and_save_polymer(self, smiles_A: str = None, smiles_B: str = None, mol_id_A: str = None, mol_id_B: str = None) -> str:
         """Build homopolymer or copolymer and save .xyz file."""
         
@@ -190,11 +135,11 @@ class PolymerXyzGenerator(XyzGeneratorBase):
                 return f"Skipping ID {mol_id_A}: Not an acrylate"
             
             contains_br_A = smiles_A.__contains__('Br')
-            smiles_A = self.replace_first_acrylate_cce(smiles_A, contains_br_A)
+            smiles_A = replace_first_acrylate_cce(smiles_A, contains_br_A)
 
             if self.polymer_type == 'copolymer':
                 contains_br_B = smiles_B.__contains__('Br')
-                smiles_B = self.replace_first_acrylate_cce(smiles_B, contains_br_B)
+                smiles_B = replace_first_acrylate_cce(smiles_B, contains_br_B)
             else:
                 contains_br_B = contains_br_A
                 smiles_B = smiles_A
